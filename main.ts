c7bf661dbcb85fc41cfba068affbe36daf9b45bd
julindavid <144339140+julindavid@users.noreply.github.com>
2023-09-11 13:34:52 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber > 5) {
+    if (receivedNumber >= 100000000) {
         Amount_of_players += 1
         Players_DSN.push(receivedNumber)
-        basic.showNumber(Players_DSN[Players_DSN.length])
+        basic.showNumber(receivedNumber)
     } else {
-    	
+        for (let Player_response_time = 0; Player_response_time <= 4; Player_response_time++) {
+            let value: number[] = []
+            value.insertAt(0, 0)
+        }
     }
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+})
+function Timer () {
+    let Player_response_time: number[] = [];
+    [0][0] = 0
+    Player_response_time[Game_DATA.indexOf(0)] = 0
+    basic.showNumber(control.millis())
+}
 input.onButtonPressed(Button.AB, function () {
     radio.setGroup(17)
     radio.sendNumber(control.deviceSerialNumber())
@@ -14,19 +26,20 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(Amount_of_players)
 })
+let Game_DATA: number[] = []
 let Players_DSN: number[] = []
 let Amount_of_players = 0
+radio.setFrequencyBand(17)
 radio.setGroup(17)
 let HUB_number = control.deviceSerialNumber()
 Amount_of_players = 0
 Players_DSN = []
+Game_DATA = []
 basic.forever(function () {
-    if (0 == 0) {
+    for (let värde of Game_DATA) {
     	
     }
 })
 basic.forever(function () {
-    if (HUB_number == control.deviceSerialNumber()) {
-    	
-    }
+	
 })
