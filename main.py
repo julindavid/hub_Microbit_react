f9a54b66f7b45137c906c47bbd7ead133b38c987
julindavid <144339140+julindavid@users.noreply.github.com>
2023-09-11 14:02:37 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-def on_received_number(receivedNumber):
-    global Player_Number, Player_List
-    Player_Number += 1
-    Player_List += 1
-radio.on_received_number(on_received_number)
-
-def f_list(list2: List[any]):
-    if 0 == 0:
-        pass
-radio.set_group(17)
-HUB_number = control.device_serial_number()
-Player_Number = 0
-Player_List = 0
-
-def on_forever():
-    if Player_List == 0:
-        basic.show_number(0)
-    else:
-        pass
-basic.forever(on_forever)
-
-def on_forever2():
-    if HUB_number == control.device_serial_number():
-        pass
-basic.forever(on_forever2)
-
-def on_forever3():
-    pass
-basic.forever(on_forever3)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber >= 100000000) {
+        Amount_of_players += 1
+        Players_DSN.push(receivedNumber)
+        basic.showNumber(receivedNumber)
+    } else {
+        for (let Player_response_time = 0; Player_response_time <= 4; Player_response_time++) {
+            let value: number[] = []
+            value.insertAt(0, 0)
+        }
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+})
+function Timer () {
+    let Player_response_time: number[] = [];
+    [0][0] = 0
+    Player_response_time[Game_DATA.indexOf(0)] = 0
+    basic.showNumber(control.millis())
+}
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(17)
+    radio.sendNumber(control.deviceSerialNumber())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Amount_of_players)
+})
+let Game_DATA: number[] = []
+let Players_DSN: number[] = []
+let Amount_of_players = 0
+radio.setFrequencyBand(17)
+radio.setGroup(17)
+let HUB_number = control.deviceSerialNumber()
+Amount_of_players = 0
+Players_DSN = []
+Game_DATA = []
+basic.forever(function () {
+    for (let värde of Game_DATA) {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
--- main.ts
+++ main.ts
@@ -2,19 +2,11 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber >= 100000000) {
         Amount_of_players += 1
         Players_DSN.push(receivedNumber)
-        basic.showNumber(receivedNumber)
     } else {
-        for (let Player_response_time = 0; Player_response_time <= 4; Player_response_time++) {
-            let value: number[] = []
-            value.insertAt(0, 0)
-        }
+        Player_response_time.push(receivedNumber)
     }
 })
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(0)
-})
-function Timer () {
-    let Player_response_time: number[] = [];
+function Timer () {;
     [0][0] = 0
     Player_response_time[Game_DATA.indexOf(0)] = 0
     basic.showNumber(control.millis())
@@ -26,6 +18,7 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(Amount_of_players)
 })
+let Player_response_time: number[] = []
 let Game_DATA: number[] = []
 let Players_DSN: number[] = []
 let Amount_of_players = 0
@@ -35,10 +28,9 @@ let HUB_number = control.deviceSerialNumber()
 Amount_of_players = 0
 Players_DSN = []
 Game_DATA = []
+let value = ["A", "B", "C"]
 basic.forever(function () {
-    for (let värde of Game_DATA) {
-    	
-    }
+	
 })
 basic.forever(function () {
 	
