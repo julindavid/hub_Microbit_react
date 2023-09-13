79e6249fffb2da27a2183b21d257651130d9248b
julindavid <144339140+julindavid@users.noreply.github.com>
2023-09-13 12:11:44 +0200

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,37 +1,47 @@
+function Skaka () {
+	
+}
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber >= 100000000) {
-        Amount_of_players += 1
-        Players_DSN.push(receivedNumber)
-    } else {
-        Player_response_time.push(receivedNumber)
+        Amount_Of_Players += 1
+        Player_DSN.unshift(receivedNumber)
+        radio.sendValue(Player_IDs.removeAt(0), receivedNumber)
     }
 })
-function Timer () {;
-    [0][0] = 0
-    Player_response_time[Game_DATA.indexOf(0)] = 0
-    basic.showNumber(control.millis())
+function Nedräkning () {
+	
 }
-input.onButtonPressed(Button.AB, function () {
-    radio.setGroup(17)
-    radio.sendNumber(control.deviceSerialNumber())
+radio.onReceivedString(function (receivedString) {
+    let Gaming_Phase = 0
+    let Setup_Phase = 0
+    if (Setup_Phase) {
+    	
+    } else if (Gaming_Phase) {
+        Player_Response_Time.unshift(0)
+        game.gameOver()
+        basic.showString("" + name + "Won!")
+        radio.sendValue(name, 100)
+    }
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(Amount_of_players)
-})
-let Player_response_time: number[] = []
-let Game_DATA: number[] = []
-let Players_DSN: number[] = []
-let Amount_of_players = 0
-radio.setFrequencyBand(17)
-radio.setGroup(17)
-let HUB_number = control.deviceSerialNumber()
-Amount_of_players = 0
-Players_DSN = []
-Game_DATA = []
-let value = ["A", "B", "C"]
-basic.forever(function () {
+radio.onReceivedValue(function (name, value) {
 	
 })
+let Player_IDs: string[] = []
+let Player_Response_Time: number[] = []
+let Player_DSN: number[] = []
+radio.setGroup(17)
+let HUB_Number = control.deviceSerialNumber()
+let Amount_Of_Players = 0
+Player_DSN = []
+Player_Response_Time = []
+Player_IDs = [
+"P1",
+"P2",
+"P3",
+"P4",
+"P5",
+"P6"
+]
 basic.forever(function () {
 	
 })
